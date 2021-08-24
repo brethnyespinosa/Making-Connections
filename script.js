6bc2da2990cb23dbfb0327f940ea93c306caf059
Brethny Espinosa <brethnyespinosa@gmail.com>
2021-08-24 00:58:55 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function removeUser(element){
+    element.remove();
+}
+
+function removeUser2(element){
+    element.remove();
+}
+
+function textchange(element){
+    element.innerText = "Jonathan Gardner"
+}
